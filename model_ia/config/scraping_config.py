3dae205dad6d374bb13cbbee93621d3c5a309a47
Nerd658 <m9bikienga@gmail.com>
2025-11-18 12:51:55 +0000

    oublie denvoie du model ia entrainer

---
+++ model_ia/config/scraping_config.py
@@ -0,0 +1,10 @@
+# Configurations globales du scraping
+
+
+MAX_POSTS_PER_SCROLL = 25   # posts détectés par scroll
+MAX_SCROLLS = 100
+
+MAX_COMMENTS_PER_POST = 200  # maximum de commentaires par post à collecter
+
+WAIT_MIN = 1
+WAIT_MAX = 3
